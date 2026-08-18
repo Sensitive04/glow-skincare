8c57b038251a57d953bf49e4a07a47362f391e4d
sensitive04 <hlla99416@gmail.com>
2026-08-19 03:06:40 +0630

    Switch to new Supabase project

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://gbsrgzezwlxubohdbuzw.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdic3JnemV6d2x4dWJvaGRidXp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwNzI1MzgsImV4cCI6MjEwMjY0ODUzOH0.0BqYuYGNVzDlfrXrRyB8zEhMTIuKWOfaDmBdakTZ8l0";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
